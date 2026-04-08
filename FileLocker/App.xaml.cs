46fe0f0df382ae9ecdb0b6ff94c8342483b08631
Jeremy <mcjeremyhaynes@gmail.com>
2026-04-07 23:33:22 -0500

    Initial FileLocker import

---
+++ FileLocker/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Microsoft.UI.Xaml;
+
+namespace FileLocker
+{
+    public partial class App : Application
+    {
+        private Window? _window;
+
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            _window = new MainWindow();
+            _window.Activate();
+        }
+    }
+}
